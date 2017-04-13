9c76f47620e1e9d700b76c02e0a0d2dac8cd9d32
palexis3 <alexispatrick77@gmail.com>
2017-04-12 17:23:22 -0700

    Added view functionality for movie adapter

--- app/src/main/java/com/example/palexis3/movieflicks/Adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/example/palexis3/movieflicks/Adapters/MovieArrayAdapter.java
@@ -1,5 +1,53 @@
 package com.example.palexis3.movieflicks.Adapters;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MovieArrayAdapter {
+import com.example.palexis3.movieflicks.Models.Movie;
+import com.example.palexis3.movieflicks.R;
+
+import java.util.List;
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // get the data item for this position
+        Movie movie = getItem(position);
+
+        // check to see if the existing view is being reused
+        if(convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        // find the image view
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+
+        // clear out image view
+        imageView.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverView = (TextView) convertView.findViewById(R.id.tvOverView);
+
+        // populate the text views
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverView.setText(movie.getOverView());
+
+        //return the view
+        return convertView;
+    }
 }
--- app/src/main/java/com/example/palexis3/movieflicks/MainFlicksActivity.java
+++ app/src/main/java/com/example/palexis3/movieflicks/MainFlicksActivity.java
@@ -3,7 +3,9 @@ package com.example.palexis3.movieflicks;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
+import com.example.palexis3.movieflicks.Adapters.MovieArrayAdapter;
 import com.example.palexis3.movieflicks.Models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -19,17 +21,30 @@ import cz.msebera.android.httpclient.Header;
 public class MainFlicksActivity extends AppCompatActivity {
 
     ArrayList<Movie> movieList;
+    MovieArrayAdapter movieAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_flicks);
 
+        // instantiating list view
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+
+        movieList = new ArrayList<>();
+
+        //instantiating movie array adapter
+        movieAdapter = new MovieArrayAdapter(this, movieList);
+
+        //setting movie adapter
+        lvItems.setAdapter(movieAdapter);
+
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=f1e55cc01616b64d1b66566ca00d707a";
 
         AsyncHttpClient client = new AsyncHttpClient();
 
-        //make a client request
+        // making an asynchronous http request
         client.get(url, new JsonHttpResponseHandler() {
 
             @Override
@@ -37,7 +52,8 @@ public class MainFlicksActivity extends AppCompatActivity {
                 JSONArray movieJSONResults = null;
                 try {
                     movieJSONResults = response.getJSONArray("results");
-                    movieList = Movie.fromJSONArray(movieJSONResults);
+                    movieList.addAll(Movie.fromJSONArray(movieJSONResults));
+                    movieAdapter.notifyDataSetChanged();
                     Log.d("DEBUG", movieList.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
