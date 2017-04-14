75e00cb423b9884130345db1a0bf0e5d9cf21df4
palexis3 <alexispatrick77@gmail.com>
2017-04-14 16:13:22 -0700

    Added xml view for landscape orientation

--- app/src/main/java/com/example/palexis3/movieflicks/Adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/example/palexis3/movieflicks/Adapters/MovieArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.example.palexis3.movieflicks.Adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -21,6 +22,8 @@ import static com.example.palexis3.movieflicks.R.id.tvTitle;
 
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
+    private Context context; // used to determine current activity
+
     // view holder cache class
     private static class ViewHolder {
         TextView title;
@@ -30,6 +33,7 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
     public MovieArrayAdapter(Context context, List<Movie> movies) {
         super(context, R.layout.item_movie, movies);
+        this.context = context;
     }
 
     // override arrayAdapter getView to populate movie items
@@ -70,8 +74,19 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         viewHolder.title.setText(movie.getOriginalTitle());
         viewHolder.overView.setText(movie.getOverView());
 
+        // get the current orientation to determine the appropriate image
+        int orientation = context.getResources().getConfiguration().orientation;
+        String imagePath = null;
+
+        // determine correct path based on orientation
+        if(orientation == Configuration.ORIENTATION_PORTRAIT) {
+            imagePath = movie.getPosterPath();
+        } else if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            imagePath = movie.getBackdropPath();
+        }
+
         // populate image using picasso
-        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.image);
+        Picasso.with(getContext()).load(imagePath).into(viewHolder.image);
 
         // return the view
         return convertView;
